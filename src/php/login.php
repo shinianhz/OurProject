2060ce882cc5970dd02159c9df8e2134eaab8d4d
pumpkinRosy <965373323@qq.com>
2018-10-10 22:46:39 +0800

    登录注册2

--- src/php/login.php
+++ src/php/login.php
@@ -4,13 +4,13 @@
     ///////
     header("Content-type:text/html;charset=utf-8");
      error_reporting(E_ALL ^ E_DEPRECATED);
-     $db = mysql_connect("localhost","root","1292"); //连接数据库   (地址 用户名 密码)
-     mysql_select_db("wangyiyanxuan",$db); //选择数据库 数据库名称 连接
+     $db = mysql_connect("localhost","root","4869"); //连接数据库   (地址 用户名 密码)
+     mysql_select_db("xihu",$db); //选择数据库 数据库名称 连接
      mysql_query("set names utf8");//设置编码格式
 
-     $uname = $_REQUEST["username"];//用户名
-     $uidcard= $_REQUEST["idcard"];//用户身份证
-     $unickname = $_REQUEST["nickname"];//用户昵称
+    //  $uname = $_REQUEST["username"];//用户名
+    //  $uidcard= $_REQUEST["idcard"];//用户身份证
+    //  $unickname = $_REQUEST["nickname"];//用户昵称
      $upwd= $_REQUEST["password"];//用户密码
      $utel = $_REQUEST["tel"];//用户手机号
 
@@ -18,10 +18,11 @@
      $data = mysql_query($sql1);//执行sql语句
 
     $result = mysql_fetch_array($data); //将查询结果提取成数组
+    // print_r($result);
     if($result){
-        echo '{"status":200,"msg":"用户名已存在 请直接登陆","res":false}';
+        echo '{"status":200,"msg":"该账号已注册","res":false}';
     }else{
-        $sql2 = "INSERT INTO `users`(`u_id`, `u_name`,`u_idcard`, `u_nickname`, `u_pwd`,`u_tel`,`u_qq`,`u_wechat`) VALUES (NULL,'$uname','$uidcard','$unickname','$upwd','$utel',NULL,NULL)";
+        $sql2 = "INSERT INTO `users`(`u_id`,`u_pwd`,`u_tel`) VALUES (NULL,'$upwd','$utel')";
         $count = mysql_query($sql2); // 增加 删除 修改 返回受影响的行数
         if($count==1){
             echo '{"status":200,"msg":"注册成功","res":true}';
